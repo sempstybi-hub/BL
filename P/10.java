40b1dbe548a5ca7fe07987f9c20e880702c2fe6b
sempstybi-hub <semp.stybi@gmail.com>
2026-06-24 21:15:38 +0300

    s

--- P/10.java
+++
@@ -1,8 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    // поиск всех пользователей по имени
-    List<User> findByName(String name);
-}
